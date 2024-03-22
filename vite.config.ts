334e5b7d3a65dea4e49a92b00bcf78328aef1baf
ogi-bulj <ognjen1802@gmail.com>
2024-03-22 15:06:11 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ogi-bulj.github.io/purchase-contracts-react",
+  base: "/purchase-contracts-react",
 });
